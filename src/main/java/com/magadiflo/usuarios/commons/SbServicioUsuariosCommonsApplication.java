5cf7fa27da778c24aed35772c0b1c6db48d075ab
Martín <magadiflo@gmail.com>
2022-09-20 17:52:45 -0500

    Excluyendo la autoconfiguración de Spring Data JPA

--- src/main/java/com/magadiflo/usuarios/commons/SbServicioUsuariosCommonsApplication.java
+++ src/main/java/com/magadiflo/usuarios/commons/SbServicioUsuariosCommonsApplication.java
@@ -1,13 +1,11 @@
 package com.magadiflo.usuarios.commons;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class }) //Excluimos la autoconfiguración de Spring Data JPA ya que por defecto pedirá una conexión
 public class SbServicioUsuariosCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SbServicioUsuariosCommonsApplication.class, args);
-	}
 
 }
